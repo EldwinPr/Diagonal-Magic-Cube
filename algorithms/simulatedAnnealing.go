c167680beb29f1ee75020bf9c9e26baf783cd413
Eldwin Pradipta <EldwinPradipta670@gmail.com>
2024-10-22 08:56:27 +0700

    Revert "bruh"
    This reverts commit 5669e1470d78cf913c9a7f257b439b7e62bf9487.

--- algorithms/simulatedAnnealing.go
+++ algorithms/simulatedAnnealing.go
@@ -14,16 +14,15 @@ func SimulatedAnnealing(cube [5][5][5]int) [5][5][5]int {
 	// helper function to update temperature
 	Schedule := func(T float64, iteration int) float64 {
 		// Update temperature based on iteration number
+
 		if iteration < 1000 {
-			return T * 0.99999999
+			return T * 0.99
 		} else if iteration < 10000 {
-			return T * 0.999999
-		} else if iteration < 100000 {
 			return T * 0.9999
-		} else if iteration < 1000000 {
-			return T * 0.99999
+		} else if iteration < 100000 {
+			return T * 0.99995
 		} else {
-			return T * 0.999999
+			return T * 0.999993
 		}
 	}
 
@@ -32,10 +31,6 @@ func SimulatedAnnealing(cube [5][5][5]int) [5][5][5]int {
 		T = Schedule(T, iteration) // Update temperature
 		iteration++
 		if T < 0.0005 { // Exit if temperature is too low
-			exit = true
-		}
-
-		if objectiveFunction.OF(cube) == 0 { // Exit if solution is found
 			exit = true
 		}
 
